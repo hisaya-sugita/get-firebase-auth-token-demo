29d58c56653f2f7bd6227e6fed40e08705ac25ef
HisayaSugita( kino ) <pswty180@gmail.com>
2021-05-28 12:11:17 +0900

    fix: detail

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
+document.getElementById('result-title').style.display = 'none';
+document.getElementById('token').style.display = 'none';
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
       authResult.user.getIdToken(/* forceRefresh */ true).then(function (idToken) {
-        sessionStorage.setItem('token', idToken);
+        document.getElementById('result-title').style.display = "";
+        document.getElementById('token').style.display = "";
+        document.getElementById('token').textContent = idToken;
         console.log(idToken);
       }).catch(function (error) {
         console.log(error);
@@ -16,7 +20,7 @@ var uiConfig = {
     }
   },
   signInFlow: 'popup',
-  signInSuccessUrl: 'success.html',
+  // signInSuccessUrl: 'success.html',
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
